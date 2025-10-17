5696f91fd39c6ca91f8681a0b732a9347691412f
m <miccoleys@gmail.com>
2025-10-17 13:52:29 +0800

    Update auth.routes.js
    added check-username routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const authController = require("../controller/authController");
 const auth = require("../middleware/auth");
-const { loginLimiter } = require("../middleware/rateLimiter");
+const loginLimiter = require("../middleware/rateLimiter");
 
 const authRouter = express.Router();
 
 authRouter.post("/login", loginLimiter, authController.login);
 
 authRouter.post("/register", authController.register);
+
+authRouter.get("/check-username", authController.checkUsername);
 
 authRouter.post("/logout", auth.requireAuth, authController.logout);
 
@@ -18,6 +20,5 @@ authRouter.post("/refresh-token", authController.refresh);
 authRouter.post("/verify-mfa", loginLimiter, authController.verifyMFA);
 
 authRouter.post("/resend-mfa", authController.resendMFA);
-
 
 module.exports = authRouter;
